7679187b2a6b08fb84dfe630a58c79eafcda5b9d
Tsura <kota@vaga.tk>
2020-06-09 13:56:43 -0500

    MobX works

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,26 +1,29 @@
 import React from "react";
 import ArticleContainer from "./ArticleContainer";
-// import store from "./DataStore.js";
-// import Axios from "axios";
+import store from "./DataStore.js";
+import { observer } from 'mobx-react'
+import Axios from "axios";
 
-class Articles extends React.Component {
-//   populateStore = () => {
-//     Axios.get("http://localhost:3001/articles").then(
-//       (articles) => (store.articles = articles)
-//     );
-//   };
+const Articles = observer(
+  class Articles extends React.Component {
+    populateStore = () => {
+      Axios.get("http://localhost:3001/articles").then(
+        (articles) => (store.articles = articles.data)
+      );
+    };
 
-//   componentDidMount() {
-//     this.populateStore();
-//   }
-  render() {
-    // console.log(this.props.store)
-    return (
-      <div>
-        <ArticleContainer store={this.props.store}/>
-      </div>
-    );
+    componentDidMount() {
+      this.populateStore();
+    }
+    render() {
+      // console.log(this.props.store)
+      return (
+        <div>
+          <ArticleContainer store={store} />
+        </div>
+      );
+    }
   }
-}
+);
 
 export default Articles;
