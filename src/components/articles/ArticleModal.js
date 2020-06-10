07709d8bc507050037f6b3de580d276864ad23d9
Tsura <kota@vaga.tk>
2020-06-10 00:18:36 -0500

    Modal trigger is custom component

--- src/components/articles/ArticleModal.js
+++ src/components/articles/ArticleModal.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { Modal } from "semantic-ui-react";
-// import ArticlePost from "./ArticlePost";
+import ArticlePost from "./ArticlePost";
 
 const ArticleModal = (props) => (
-  <Modal trigger={<p>{`${props.article.url}`}</p>}>
+  <Modal trigger={<ArticlePost article={props.article} {...props} />}>
     <Modal.Header>{/*insert article title here*/}</Modal.Header>
     <Modal.Content image>
       <Modal.Description>
